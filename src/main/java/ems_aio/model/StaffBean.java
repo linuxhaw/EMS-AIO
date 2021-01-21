138ef1f36d5ecbb348a4495e17a75786333a9c48
Aung Kaung Myat <75575399+AungKaungMyat1395@users.noreply.github.com>
2021-01-21 16:22:49 +0630

    ADMIN_DESIGN_AKM

---
+++ src/main/java/ems_aio/model/StaffBean.java
@@ -0,0 +1,17 @@
+package ems_aio.model;
+
+public class StaffBean {
+	private String txtstaffId;
+	private String txtstaffName;
+	private String txtstaffNRC;
+	private String txtstaffEmail;
+	private String txtstaffPhone;
+	private String txtstaffSalary;
+	private String txtstaffBankAcc;
+	private String selstaffBankName;
+	private String txtstaffAddress;
+	private String txtstaffBirthday;
+	private String txtstaffRegisterDate;
+	private String rodstaffGender;
+	
+}

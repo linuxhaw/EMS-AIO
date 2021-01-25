657aeebe99d3ab8c3ac0d40650c70a579fa2e4a0
ZayBoHein <zaybohein@gmail.com>
2021-01-25 14:22:58 +0630

    ZAYBOHEIN

--- src/main/java/ems_aio/dto/MEMP001.java
+++ src/main/java/ems_aio/dto/MEMP001.java
@@ -1,5 +1,8 @@
 package ems_aio.dto;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,14 +12,42 @@ import javax.persistence.Table;
 @Table
 public class MEMP001 {
 	@Id
-	@Column(name="rol_id")
-	private String rolid;
-	@Column(name="rol_name")
-	private String rolname;
-	@Column(name="rol_create")
-	private String createdate;
+	@Column(name="emp_id")
+	private String id;
+	@Column(name="emp_name")
+	private String name;
+	@Column(name="emp_address")
+	private String address;
+	@Column(name="emp_phone")
+	private String phone;
+	@Column(name="emp_birthday")
+	private Date birthday;
+	@Column(name="emp_gender")
+	private String gender;
+	@Column(name="emp_marrage")
+	private String marrage;
+	@Column(name="emp_religion")
+	private String religion;
+	@Column(name="emp_nationality")
+	private String nation;
+	@Column(name="emp_password")
+	private String password;
+	@Column(name="emp_payroll")
+	private double payroll;
+	@Column(name="emp_bnkacc")
+	private String bnkacc;
+	@Column(name="emp_bnk")
+	private String bank;
+	@Column(name="emp_pos")
+	private String pos;
+	@Column(name="emp_dep")
+	private String dep;
+	@Column(name="emp_rol")
+	private String rol;
+	@Column(name="dep_create")
+	private Timestamp createdate;
 	@Column(name="rol_update")
-	private String updatedate;
+	private Timestamp updatedate;
 	@Column(name="rol_status")
 	private boolean status;
 
